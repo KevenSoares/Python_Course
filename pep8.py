60a31510383d781b45f0b98fee84e9ed9118bbcc
Keven Soares <keven.soaresmao@gmail.com>
2019-10-16 21:28:05 -0400

    Initial commit.

---
+++ pep8.py
@@ -0,0 +1,21 @@
+"""
+PEP8 - PYTHON ENHANCMENTS PROPOSAL
+
+SÃO PROPOSTAS DE MELHORIAS PARA A LINGUAGEM PYTHON
+
+The Zen of python
+
+import this
+
+a ideia da PEP8 é que possamos escrever códigos em Python, de forma pyhtonica.
+
+[1] - utilize camel case para nomes de classes;
+[2] - variáveis e funções em minúsculo
+[3] - utilize 4 espaços para identação
+[4] - linhas em branco
+[5] - imports devem ser em linhas separadas
+[6] - não deixe espaços em nomes de funções ou comandos, ou expressoes
+[7] - sempre termine uma instrução com uma nova linha
+"""
+
+import this
