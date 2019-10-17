fb5d7210ea36ab381ce42dbf5d5b3fa39ece9988
Keven Soares <keven.soaresmao@gmail.com>
2019-10-17 12:57:38 -0400

    Conditional

---
+++ 20_cond_if_else_elif.py
@@ -0,0 +1,14 @@
+"""
+conditional structures: if else elif
+
+"""
+
+age = int(input("idade: "))
+
+if age < 18:
+    print("menor de idade")
+elif age >= 18:
+    print("adult")
+else:
+    print("dont care")
+
