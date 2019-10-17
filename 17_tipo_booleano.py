49c15bf71a8aa55c22ef7a8f6b391c5c467c5ece
Keven Soares <keven.soaresmao@gmail.com>
2019-10-17 10:41:33 -0400

    Working with Boolean variable

---
+++ 17_tipo_booleano.py
@@ -0,0 +1,34 @@
+"""
+Boolean Type
+
+True
+False
+
+always use T and F in uppercase
+
+"""
+
+active = True
+deactivate = False
+
+print(active)
+
+#  negate (not)
+
+print(not active)
+
+# or (or)
+
+print(deactivate or deactivate)
+print(deactivate or active)
+print(active or deactivate)
+print(active or active)
+
+# and (and)
+
+print(deactivate and deactivate)
+print(deactivate and active)
+print(active and deactivate)
+print(active and active)
+
+
