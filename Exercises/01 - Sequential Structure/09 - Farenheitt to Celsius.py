22763c143297d05de514c34689b7436c53f7d8c4
Keven Soares <keven.soaresmao@gmail.com>
2019-10-29 15:42:47 -0400

    Exercise 9

---
+++ Exercises/01 - Sequential Structure/09 - Farenheitt to Celsius.py
@@ -0,0 +1,9 @@
+"""
+Exercise
+
+9 - Make a Program that asks for temperature in degrees Farenheit, transforms and shows temperature in degrees Celsius
+
+"""
+
+tempf = int(input("Enter the temperature in Farenheitt: "))
+print(f'temperature in celsius is {(5*(tempf-32))/9} °C')
