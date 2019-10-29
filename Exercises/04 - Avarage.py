ef11f73d8cbda485a93fdfc4da466f6eac7607bf
Keven Soares <keven.soaresmao@gmail.com>
2019-10-29 14:10:38 -0400

    Exercise 4

---
+++ Exercises/04 - Avarage.py
@@ -0,0 +1,11 @@
+"""
+Exercise
+
+4 - Make a program that asks for the 4 bimonthly notes and shows the average.
+
+"""
+n1 = float(input("Insert n1: "))
+n2 = float(input("Insert n2: "))
+n3 = float(input("Insert n3: "))
+n4 = float(input("Insert n4: "))
+print((n1+n2+n3+n4)/4)
