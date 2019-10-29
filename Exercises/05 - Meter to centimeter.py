991d24880fa166598036142124bcc50cfca976f6
Keven Soares <keven.soaresmao@gmail.com>
2019-10-29 14:15:39 -0400

    Exercise 5

---
+++ Exercises/05 - Meter to centimeter.py
@@ -0,0 +1,9 @@
+"""
+Exercise
+
+5 - Make a program that converts meters to centimeters
+
+"""
+
+meter = float(input("Insert a measure in meter: "))
+print(f'the measure in centimeters is {meter*100} cm')
