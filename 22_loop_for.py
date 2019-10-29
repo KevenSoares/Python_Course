0d58a2907783f08b4d4b041f5d08f3b32b601a70
Keven Soares <keven.soaresmao@gmail.com>
2019-10-29 13:40:21 -0400

    Loop for

---
+++ 22_loop_for.py
@@ -0,0 +1,22 @@
+"""
+Loop for
+
+loop -> estrutura de repetição
+
+for item in interavel
+    execucao do loop
+utilizamos loops para iterar sobre sequencias ou sobre valores iteraveis
+
+"""
+
+#  ex 1
+nome = 'kev'
+lista = [1, 3, 5, 7]
+numeros = range(1, 10)
+
+for letra in nome:
+    print(letra)
+for numero in lista:
+    print(numero)
+for numero in range(1, 10):
+    print(numero)
