f50a77e4a58898db1d03f7501a94f3d7bb39f618
Keven Soares <keven.soaresmao@gmail.com>
2019-10-29 16:00:32 -0400

    Exercise 11

---
+++ Exercises/01 - Sequential Structure/11 - Multinumbers calculator.py
@@ -0,0 +1,16 @@
+"""
+Exercise
+
+11 - Make a program that asks for 2 integers and a real number. Calculate and show:
+11.1 - the product of double the first with half of the second.
+11.2 - the sum of the triple of the first with the third.
+11.3 - the third cubed.
+
+"""
+n1 = int(input("Enter n1: "))
+n2 = int(input("Enter n2: "))
+n3 = float(input("Enter n3: "))
+
+print((n1*2)*(n2/2))
+print((3*n1)+n3)
+print(n3**3)
