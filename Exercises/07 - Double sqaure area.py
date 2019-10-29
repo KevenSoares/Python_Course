a00f937b2a74971535ed59c4a29d029a490a9a1c
Keven Soares <keven.soaresmao@gmail.com>
2019-10-29 14:31:32 -0400

    Exercise 7

---
+++ Exercises/07 - Double sqaure area.py
@@ -0,0 +1,9 @@
+"""
+Exercise
+
+7 - Make a program that calculates the area of ​​a square, then show twice that area to the user.
+
+"""
+
+side = float(input("Enter the size of the side of the square: "))
+print(f'The double of the area is {(side**2)*2}')
