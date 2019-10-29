bd714bcce1d40565756880c4ad156f11513b0eeb
Keven Soares <keven.soaresmao@gmail.com>
2019-10-29 13:39:17 -0400

    Exercise 2

---
+++ Exercises/02 - Receive and print number.py
@@ -0,0 +1,11 @@
+"""
+Exercise
+
+2 - Ask the user to insert a number, then print this number in a message:
+
+The entered number was: [NUMBER]
+
+"""
+num = input("Enter a number: ")
+print(f'The entered number was: {num}')
+
