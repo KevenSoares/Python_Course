07c89f233c89306c54b10e1d1b014d9afd118ca0
Keven Soares <keven.soaresmao@gmail.com>
2019-10-29 14:22:04 -0400

    Exercise 6

---
+++ Exercises/06 - Circle area.py
@@ -0,0 +1,9 @@
+"""
+Exercise
+
+6 - Make a program that asks for the radius of a circle, calculates and shows its area.
+
+"""
+
+radius = float(input("Enter the circles radius: "))
+print(f'The circle area is: {radius*2*3.14}')
