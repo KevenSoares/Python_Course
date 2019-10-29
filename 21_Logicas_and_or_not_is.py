04a07d61a3032bdf9c37f2e06d56d315d10253a3
Keven Soares <keven.soaresmao@gmail.com>
2019-10-29 13:40:01 -0400

    Logic(AND, OR, NOT, IS)

---
+++ 21_Logicas_and_or_not_is.py
@@ -0,0 +1,16 @@
+"""
+    Estruturas Lógicas
+    and
+    or
+    not
+    is 
+
+"""
+
+active = True
+logged = True
+
+if active and logged:
+    print("Hello World")
+else:
+    print("Voce precisa acessar sua conta")
