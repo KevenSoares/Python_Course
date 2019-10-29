51807d930899c1f1c9fb1d69cf9f5f0a4618a23d
Keven Soares <keven.soaresmao@gmail.com>
2019-10-29 16:09:58 -0400

    Exercise 12

---
+++ Exercises/01 - Sequential Structure/12 - Ideal Weight.py
@@ -0,0 +1,10 @@
+"""
+Exercise
+
+11 - Taking input as a person's height, construct an algorithm that calculates their ideal weight using the following
+     formula: (72.7 * height) - 58
+
+"""
+
+height = float(input("Enter your height: "))
+print(f'Your ideal wight is {(72.7 * height) - 58}')
