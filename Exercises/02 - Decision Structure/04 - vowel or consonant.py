c41f0928d20f15e84c3948b819c7cea30094b420
Keven Soares <keven.soaresmao@gmail.com>
2019-10-30 10:52:57 -0400

    Exercise 04

---
+++ Exercises/02 - Decision Structure/04 - vowel or consonant.py
@@ -0,0 +1,13 @@
+"""
+Exercises
+
+04 - Make a program that checks if a letter you type is vowel or consonant.
+"""
+
+letter = input('Type a letter: ')
+
+if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u' or letter == 'A' or letter == 'E' \
+        or letter ==  'I' or letter == 'O' or letter == 'U':
+    print("Vowel")
+else:
+    print("Consonant")
