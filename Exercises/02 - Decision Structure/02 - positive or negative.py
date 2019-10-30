8d610928d270301ed8a25defbaaa59185ec92d22
Keven Soares <keven.soaresmao@gmail.com>
2019-10-30 09:58:20 -0400

    Exercise 02

---
+++ Exercises/02 - Decision Structure/02 - positive or negative.py
@@ -0,0 +1,14 @@
+"""
+Exercises
+
+02 - Make a program that asks for a value and shows on the screen whether the value is positive or negative.
+
+"""
+
+n = float(input("Enter a number: "))
+
+if n > 0:
+    print("positive")
+else:
+    print("negative")
+
