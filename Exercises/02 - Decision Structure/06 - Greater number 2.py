315ac3dc2184bd9d71d0a06ec41b642965133768
Keven Soares <keven.soaresmao@gmail.com>
2019-11-20 09:30:36 -0400

    Exercise 06 - Correction

--- Exercises/02 - Decision Structure/06 - Greater number 2.py
+++ Exercises/02 - Decision Structure/06 - Greater number 2.py
@@ -9,9 +9,9 @@ n1 = float(input("Enter the 1st number: "))
 n2 = float(input("Enter the 2nd number: "))
 n3 = float(input("Enter the 3th number: "))
 
-if n1 > n2 > n3:
+if n1 > n2 > n3 or n1 > n3 > n2:
     print(f"the greater is: {n1}")
-elif n2 > n1 > n3:
+elif n2 > n1 > n3 or n2 > n3 > n1:
     print(f"the greater is: {n2}")
 else:
     print(f"the greater is: {n3}")
