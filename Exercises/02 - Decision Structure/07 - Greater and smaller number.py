7eca8bad033461cc829d410c244d726198dc5800
Keven Soares <keven.soaresmao@gmail.com>
2019-11-20 09:30:50 -0400

    Exercise 07 - Correction

--- Exercises/02 - Decision Structure/07 - Greater and smaller number.py
+++ Exercises/02 - Decision Structure/07 - Greater and smaller number.py
@@ -12,9 +12,15 @@ n3 = float(input("Enter the 3th number: "))
 if n1 > n2 > n3:
     print(f"the greater is: {n1}")
     print(f"the smaller is: {n3}")
+elif n1 > n3 > n2:
+    print(f"the greater is: {n1}")
+    print(f"the smaller is: {n2}")
 elif n2 > n1 > n3:
     print(f"the greater is: {n2}")
     print(f"the smaller is: {n3}")
+elif n2 > n3 > n1:
+    print(f"the greater is: {n2}")
+    print(f"the smaller is: {n1}")
 else:
     print(f"the greater is: {n3}")
     if n1 > n2:
